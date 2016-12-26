a9a51d8169339c65e24df4c4d5868412f87998fb
Susheel Mannepalli <susheel.mannepalli@gmail.com>
2016-12-25 23:51:13 -0800

    Initial commit

--- app/app.jsx
+++ app/app.jsx
@@ -2,12 +2,14 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+var TodoApp = require('TodoApp');
+
 //load foundation
 $(document).foundation();
 //load custom css
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-  <p>Boiler plate 3 project</p>,
+  <TodoApp/>,
   document.getElementById('app')
 );
