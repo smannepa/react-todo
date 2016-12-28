ac476de3eb5ad3d46b6c9ff27ee86d00432f3065
Susheel Mannepalli <susheel.mannepalli@gmail.com>
2016-12-27 16:06:36 -0800

    added addTodo and toggleTodo reducers

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -23,4 +23,32 @@ describe('Reducers', () => {
       expect(res).toEqual(false);
     });
   });
+  describe('todosReducer', () => {
+    it('Should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Read a book'
+      };
+      var res = reducers.todosReducer(df([]), df(action));
+      expect(res.length).toBe(1);
+      expect(res[0].text).toBe(action.text);
+    });
+    it('Should toggle a todo', () => {
+      var todos = [{
+        id: 124,
+        text: 'todo to be complted',
+        completed: true,
+        createdAt: 123,
+        completedAt: 345
+      }];
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: 124
+      };
+      var res = reducers.todosReducer(df(todos), df(action));
+
+      expect(res[0].completed).toBe(false);
+      expect(res[0].completedAt).toBe(undefined);
+    });
+  });
 });
